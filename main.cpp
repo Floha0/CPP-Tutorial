8c4688e4f300592ffddde41281dbe5e0b038320f
Furkan Gülhan <fgulhan07@gmail.com>
2024-09-12 03:03:18 +0300

    First Commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    string name;
+    name = "Furkan";
+
+    cout << "My Name is  " << name << endl;
+    cout << "And I'm not " << pow(2,4) << " years old" << endl;
+    return 0;
+}
