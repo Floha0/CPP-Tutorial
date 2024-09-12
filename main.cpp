fad34cedc1ba724a57a1eb58864575b283acc693
Furkan Gülhan <fgulhan07@gmail.com>
2024-09-12 03:12:49 +0300

    Input

--- main.cpp
+++ main.cpp
@@ -9,5 +9,15 @@ int main() {
 
     cout << "My Name is  " << name << endl;
     cout << "And I'm not " << pow(2,4) << " years old" << endl;
+
+    int age = 15;
+    cin >> age;
+
+    string yourName;
+    getline(cin, yourName);
+    // A (bug) occurs ig? Program asks me for only age but name
+
+    cout << "You are " << age << " years old!" << " And your name is: "<< yourName << endl;
+
     return 0;
 }
